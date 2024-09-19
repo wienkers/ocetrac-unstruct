a782b1e44a2f52bb598353bae347639bd6d41d57
Aaron Wienkers <aaron.wienkers@gmail.com>
2024-09-19 15:14:18 +0200

    Fixes for pip.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 
 setup(
+    name='ocetrac-dask',
     use_scm_version={
-        "write_to": "ocetrac/_version.py",
+        "write_to": "ocetrac_dask/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     }
